cc13406bc86f156fb8bf7d94e9ea90c12ea0f26d
parimal-kini <99535682+parimal-kini@users.noreply.github.com>
2022-09-23 04:23:09 +0530

    Time: 14 ms, Memory: 56 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -4,17 +4,17 @@ class Solution {
         for(int i = 0; i< nums.length; i++)
             sqrArr[i] = nums[i] * nums[i];
         
-        // Arrays.sort(sqrArr);
-        // return sqrArr;
-        for(int i = 1; i < sqrArr.length; i++){
-            for(int j = 0; j < sqrArr.length; j++){
-                if(sqrArr[j] > sqrArr[i]){
-                    int temp = sqrArr[i];
-                    sqrArr[i] = sqrArr[j];
-                    sqrArr[j] = temp;
-                }
-            }
-        }
+        Arrays.sort(sqrArr);
         return sqrArr;
+        // for(int i = 1; i < sqrArr.length; i++){
+        //     for(int j = 0; j < sqrArr.length; j++){
+        //         if(sqrArr[j] > sqrArr[i]){
+        //             int temp = sqrArr[i];
+        //             sqrArr[i] = sqrArr[j];
+        //             sqrArr[j] = temp;
+        //         }
+        //     }
+        // }
+        // return sqrArr;
     }
 }
